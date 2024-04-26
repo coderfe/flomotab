a5a8df30426137cfd6dfb6816d9abea5256b7a43
coderfee <coderfee@outlook.com>
2024-04-26 10:51:11 +0800

    移除未使用权限 :zap:

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,64 +1,31 @@
-import { onMessage, sendMessage } from "webext-bridge/background";
-import type { Tabs } from "webextension-polyfill";
+import { onMessage } from 'webext-bridge/background'
 
 // only on dev mode
 if (import.meta.hot) {
   // @ts-expect-error for background HMR
-  import("/@vite/client");
+  import('/@vite/client')
   // load latest content script
-  import("./contentScriptHMR");
+  import('./contentScriptHMR')
 }
 
 browser.runtime.onInstalled.addListener((): void => {
   // eslint-disable-next-line no-console
-  console.log("Flomo Tab installed");
-});
+  console.log('flomotab installed')
+})
 
-let previousTabId = 0;
-
-// communication example: send previous tab title from background page
-// see shim.d.ts for type declaration
-browser.tabs.onActivated.addListener(async ({ tabId }) => {
-  if (!previousTabId) {
-    previousTabId = tabId;
-    return;
-  }
-
-  let tab: Tabs.Tab;
-
-  try {
-    tab = await browser.tabs.get(previousTabId);
-    previousTabId = tabId;
-  } catch {
-    return;
-  }
-});
-
-onMessage("get-current-tab", async () => {
-  try {
-    const tab = await browser.tabs.get(previousTabId);
-    return {
-      title: tab?.title,
-    };
-  } catch {
-    return {
-      title: undefined,
-    };
-  }
-});
-
-onMessage("sync-memos", ({ data: { memos } }) => {
+onMessage('sync-memos', ({ data: { memos } }) => {
   browser.storage.local.set({
-    "flomo-tab-memos": memos,
-  });
-});
+    'flomo-tab-memos': memos,
+  })
+})
 
 browser.action.onClicked.addListener(async (tab) => {
-  if (!tab.url?.includes("v.flomoapp.com")) return;
+  if (!tab.url?.includes('v.flomoapp.com'))
+    return
   browser.scripting.executeScript({
     target: {
       tabId: tab.id!,
     },
-    files: ["dist/contentScripts/index.global.js"],
-  });
-});
+    files: ['dist/contentScripts/index.global.js'],
+  })
+})
--- src/manifest.ts
+++ src/manifest.ts
@@ -1,10 +1,10 @@
-import fs from "fs-extra";
-import type { Manifest } from "webextension-polyfill";
-import type PkgType from "../package.json";
-import { isDev, isFirefox, port, r } from "../scripts/utils";
+import fs from 'fs-extra'
+import type { Manifest } from 'webextension-polyfill'
+import type PkgType from '../package.json'
+import { isDev, isFirefox, port, r } from '../scripts/utils'
 
 export async function getManifest() {
-  const pkg = (await fs.readJSON(r("package.json"))) as typeof PkgType;
+  const pkg = (await fs.readJSON(r('package.json'))) as typeof PkgType
 
   // update this file to update this manifest.json
   // can also be conditional based on your need
@@ -13,60 +13,54 @@ export async function getManifest() {
     name: pkg.displayName || pkg.name,
     version: pkg.version,
     description: pkg.description,
+    homepage_url: pkg.homepage,
     action: {
-      default_icon: "./assets/icon-512.png",
+      default_icon: './assets/icon-512.png',
       // default_popup: "./dist/popup/index.html",
     },
     chrome_url_overrides: {
-      newtab: "./dist/newtab/index.html",
+      newtab: './dist/newtab/index.html',
     },
     background: isFirefox
       ? {
-          scripts: ["dist/background/index.mjs"],
-          type: "module",
+          scripts: ['dist/background/index.mjs'],
+          type: 'module',
         }
       : {
-          service_worker: "./dist/background/index.mjs",
+          service_worker: './dist/background/index.mjs',
         },
     icons: {
-      16: "./assets/icon-512.png",
-      48: "./assets/icon-512.png",
-      128: "./assets/icon-512.png",
+      16: './assets/icon-512.png',
+      48: './assets/icon-512.png',
+      128: './assets/icon-512.png',
     },
-    permissions: ["tabs", "storage", "activeTab", "scripting"],
-    host_permissions: ["https://v.flomoapp.com/*"],
+    permissions: ['storage', 'scripting'],
+    host_permissions: ['https://v.flomoapp.com/*'],
     content_scripts: [
       {
-        matches: ["https://v.flomoapp.com/*"],
+        matches: ['https://v.flomoapp.com/*'],
         js: [
           // "dist/contentScripts/index.global.js",
-          "./lib/toastify/toastify.js",
+          './lib/toastify/toastify.js',
         ],
-        css: ["./lib/toastify/toastify.css"],
+        css: ['./lib/toastify/toastify.css'],
       },
     ],
     web_accessible_resources: [
       {
-        resources: ["dist/contentScripts/style.css"],
-        matches: ["https://v.flomoapp.com/*"],
+        resources: ['dist/contentScripts/style.css'],
+        matches: ['https://v.flomoapp.com/*'],
       },
     ],
     content_security_policy: {
       extension_pages: isDev
-        ? // this is required on dev for Vite script to load
-          `script-src \'self\' http://localhost:${port}; object-src \'self\'`
-        : "script-src 'self'; object-src 'self'",
+        ? `script-src \'self\' http://localhost:${port}; object-src \'self\'`
+        : 'script-src \'self\'; object-src \'self\'',
     },
-  };
-
-  // FIXME: not work in MV3
-  if (isDev && false) {
-    // for content script, as browsers will cache them for each reload,
-    // we use a background script to always inject the latest version
-    // see src/background/contentScriptHMR.ts
-    delete manifest.content_scripts;
-    manifest.permissions?.push("webNavigation");
   }
 
-  return manifest;
+  if (isDev)
+    manifest.permissions?.push('webNavigation')
+
+  return manifest
 }
